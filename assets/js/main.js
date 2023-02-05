9630d158d75f0142003e4811983a4c4a4101b627
George Stepaniants <georgestepaniants@dhcp-10-29-27-192.dyn.MIT.EDU>
2023-02-05 18:09:16 -0500

    trying to revert

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,7 +208,7 @@ const openModalPortfolio = document.querySelectorAll(".open-modal-portfolio");
 const contentModalBlog = document.querySelector("#content-modal-blog");
 const openModalBlog = document.querySelectorAll(".open-modal-blog");
 
-// Open modal portfolio
+// Open Modal portfolio
 openModalPortfolio.forEach((open) => {
   open.onclick = function () {
     contentModalPortfolio.style.visibility = "visible";
